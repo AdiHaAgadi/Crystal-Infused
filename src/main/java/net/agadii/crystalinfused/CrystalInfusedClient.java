12dd45ab7b03d5cfad31490bfdb4623df63a3e7c
Adi <adiefrat071103@gmail.com>
2024-01-30 15:58:31 +0200

    init project for fabric 1.19.4

---
+++ src/main/java/net/agadii/crystalinfused/CrystalInfusedClient.java
@@ -0,0 +1,10 @@
+package net.agadii.crystalinfused;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CrystalInfusedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/agadii/crystalinfused/CrystalInfusedDataGenerator.java
@@ -0,0 +1,11 @@
+package net.agadii.crystalinfused;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CrystalInfusedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
